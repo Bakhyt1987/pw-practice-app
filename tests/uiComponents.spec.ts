f783c25d5ceee91f95ad764b6077db13d02962ad
Bakhyt1987 <b-root@list.ru>
2024-05-29 18:40:29 +0500

    working with radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,30 +1,48 @@
 import { expect, test } from "@playwright/test";
 
-test.beforeEach(async({page})=>{
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/')
 })
 
 
-test.describe('suite 1', ()=> {
-    test.beforeEach(async({page})=>{
-        await page.getByText('Forms').click()
-        await page.getByText('Form Layouts').click()
-    })
+test.describe('suite 1', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+  })
 
-    test('input fields',async({page}) => {
-      const usingTheGridEmailInput = page.locator('nb-card',{hasText:"Using the Grid"})
-      .getByRole("textbox", {name: "Email"})
+  test('input fields', async ({ page }) => {
+    const usingTheGridEmailInput = page.locator('nb-card', { hasText: "Using the Grid" })
+      .getByRole("textbox", { name: "Email" })
 
-      await usingTheGridEmailInput.fill('test@test.com')
-      await usingTheGridEmailInput.clear()
-      await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 500})
+    await usingTheGridEmailInput.fill('test@test.com')
+    await usingTheGridEmailInput.clear()
+    await usingTheGridEmailInput.pressSequentially('test2@test.com', { delay: 500 })
 
-      //generic assertion
-      const inputValue = await usingTheGridEmailInput.inputValue()
-      expect(inputValue).toEqual('test2@test.com')
+    //generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue()
+    expect(inputValue).toEqual('test2@test.com')
 
-      //locator assertion
-      await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
-    })
+    //locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+  })
+
+  test('radio buttons', async ({ page }) => {
+    const usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid" })
+
+    //await usingTheGridForm.getByLabel('Option 1').check({ force: true })
+    await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).check({ force: true })
+
+    const radioStatus = await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).isChecked()
+    expect(radioStatus).toBeTruthy()
+
+    await expect(usingTheGridForm.getByRole('radio', { name: 'Option 1' })).toBeChecked()
+
+    await usingTheGridForm.getByRole('radio', { name: 'Option 2' }).check({ force: true })
+    expect(await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).isChecked()).toBeFalsy()
+    expect(await usingTheGridForm.getByRole('radio', { name: 'Option 2' }).isChecked()).toBeTruthy()
+
+  })
+
 
 })
